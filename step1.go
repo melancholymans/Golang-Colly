a339faf293a94cb1aa69ac96bcd58fd92482eecb
joshua <kjkq26993@gaia.eonet.ne.jp>
2022-08-22 09:29:00 +0900

    最新の記事をとってくる

--- step1.go
+++ step1.go
@@ -9,13 +9,12 @@ import (
 	"github.com/gocolly/colly/v2"
 )
 
-type pageInfo struct {
-	StatusCode int    `json:"statusCode"`
-	URL        string `json:"url"`
-	Title      string `json:"title"`
+type articleInfo struct {
+	Title string `json:"title"`
+	URL   string `json:"url"`
 }
 
-func savePageJson(fName string, p *pageInfo) {
+func saveArticlesJson(fName string, a []articleInfo) {
 	// Create json file
 	file, err := os.Create(fName)
 	if err != nil {
@@ -27,13 +26,13 @@ func savePageJson(fName string, p *pageInfo) {
 	// Dump json to the standard output
 	enc := json.NewEncoder(file)
 	enc.SetIndent("", "  ")
-	err = enc.Encode(p)
+	err = enc.Encode(a)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// Struct to json
-	b, _ := json.MarshalIndent(p, "", "  ")
+	b, _ := json.MarshalIndent(a, "", "  ")
 	fmt.Println(string(b))
 	// fmt.Println(p)
 }
@@ -42,35 +41,38 @@ func main() {
 	// Target URL
 	url := "https://cpp-learning.com"
 
-	p := &pageInfo{}
-	c := colly.NewCollector()
-	c.OnHTML("title", func(e *colly.HTMLElement) {
-		p.Title = e.Text
-		fmt.Println(e.Text)
-	})
+	articles := make([]articleInfo, 0, 4)
 
-	// Before making a request print "Visiting URL: https://XXX"
+	// Instantiate default collector
+	c := colly.NewCollector()
+
+	i := 0
+	// Extract li class="new-entry-item"
+	c.OnHTML("li[class=new-entry-item]", func(e *colly.HTMLElement) {
+		i++
+		fmt.Println(i)
+		title := e.ChildText("h3")
+		fmt.Println(title)
+
+		link, _ := e.DOM.Find("a[href]").Attr("href")
+		fmt.Println(link)
+
+		article := articleInfo{
+			Title: title,
+			URL:   link,
+		}
+		articles = append(articles, article)
+	})
 	c.OnRequest(func(r *colly.Request) {
-		p.URL = r.URL.String()
 		fmt.Println("Visiting URL:", r.URL.String())
 	})
-
-	// After making a request extract status code
 	c.OnResponse(func(r *colly.Response) {
-		p.StatusCode = r.StatusCode
 		fmt.Println("StatusCode:", r.StatusCode)
 	})
 	c.OnError(func(r *colly.Response, err error) {
-		p.StatusCode = r.StatusCode
 		log.Println("error:", r.StatusCode, err)
 	})
-
-	// Start scraping on https://XXX
 	c.Visit(url)
-
-	// Wait until threads are finished
 	c.Wait()
-
-	// Save as JSON format
-	savePageJson("page.json", p)
+	saveArticlesJson("articles.json", articles)
 }
